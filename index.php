4db368083d9500ead82543127504c93b645e8d7b
Joshua Ramos <auhsojsomar@gmail.com>
2022-01-11 12:57:03 +0800

    inital commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <title>Nityo Infotech</title>
+</head>
+<body>
+    <div class="container">
+        <div class="btn btn-success" data-bs-toggle="modal" data-bs-target="#productModal">
+            Add Product
+        </div>
+
+        <div class="modal fade" id="productModal" tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Add Product</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
+            </div>
+    </div>
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
