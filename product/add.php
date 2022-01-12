7fae912c54b7062166b98a23798a1185c8d5fd74
Joshua Ramos <auhsojsomar@gmail.com>
2022-01-12 21:49:33 +0800

    add datatables

--- product/add.php
+++ product/add.php
@@ -3,15 +3,5 @@
 include "./Product.php";
 
 $product = new Product("Product Added");
-echo json_encode($product->insertData());
-// public function insertData(){
-//     if($_SERVER["REQUEST_METHOD"] == "POST"){
-//         if($output["msgType"] == "success"){
-//             $sql = "INSERT INTO products (name, unit, price, date, available, image) VALUES (?, ?, ?, ?, ?, ?)";
-//             $stmt = $conn->prepare($sql);
-//             $stmt->execute([$name, $unit, $newPrice, $date, $available, $newName]);
-//         }
-//     }
-// }
 
 ?>
